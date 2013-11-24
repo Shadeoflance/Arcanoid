31db06893774c3e765682d089f9fced6e3fb6188
Shadeoflance <shadeoflance@gmail.com>
2013-11-25 00:14:22 +0400

    Menu button on victory screen

--- Arcanoid/Menu/Victory.cs
+++ Arcanoid/Menu/Victory.cs
@@ -8,13 +8,23 @@ class Victory : State
     string bottext = "FINAL SCORE: ";
     static SystemFont font = new SystemFont("04b03", 50, FontStyle.Bold);
     Camera cam = new Camera(240);
+    Button BackToMenu = new Button();
 
     public Victory()
     {
         cam.Apply();
         font.Smooth = false;
+        BackToMenu.text = "BACK TO MENU";
+        BackToMenu.Position = new Vec2(-75, -100);
+        BackToMenu.Size = new Vec2(150, 22);
     }
-
+    public override void MouseDown(MouseButton button, Vec2 pos)
+    {
+        if (button != MouseButton.Left)
+            return;
+        if (BackToMenu.Hit())
+            App.NextState = new Menu();
+    }
     public override void Render()
     {
         Draw.Clear(Color.White);
@@ -25,10 +35,11 @@ class Victory : State
         font.Render(toptext);
         Draw.Load();
         Draw.Save();
-        Draw.Translate(new Vec2(-150, 0));
+        Draw.Translate(new Vec2(-115, 0));
         Draw.Color(new Color(0.7, 0.7, 0.7));
         Draw.Scale(20);
         font.Render(bottext + World.Current.Score.ToString());
         Draw.Load();
+        BackToMenu.Render();
     }
 }
